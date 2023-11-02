2192c82ae2ba8bfe6b321aa91f0b9b4907ad0100
joje3029 <joje3029@gmail.com>
2023-11-02 13:50:08 +0900

    첫 JSP 프로젝트

---
+++ src/main/java/com/koreaIT/java/am/gugudanServlet.java
@@ -0,0 +1,23 @@
+package com.koreaIT.java.am;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/home/gugudan")
+public class gugudanServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		
+		response.getWriter().append("Hello World~~ 뱁새");
+	}
+
+
+}
